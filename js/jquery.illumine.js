139890ae9902e802eaa5c2334410da973ba6e00a
Rodolphe Stoclin <srodolphe@gmail.com>
2010-11-07 18:41:34 +0100

    Initial release

---
+++ js/jquery.illumine.js
@@ -0,0 +1,47 @@
+﻿/* Illumine - jQuery plugin
+ * 
+ * This plugin is used to highlight elements on a web page.
+ * 
+ * How to use:
+ * $("#myElement .elements").illumine();
+ * 
+ * Developed with jQuery version: 1.4.3
+ * 
+ * Version: 1.0.0
+ * Name: illumine
+ * 
+ * Author: Rodolphe Stoclin
+ * E-mail: rodolphe@2clics.net
+ */
+ 
+(function($){
+	$.fn.extend({
+		illumine: function(options) {
+            var defaults = {
+            	opacity: 0.2,
+                animateTimer: 500
+            };
+            
+            settings = $.extend(defaults, options);
+            
+            var elements = this;
+            var timer;
+            
+            return elements.each(function(){
+                var elt = jQuery(this);
+                
+                elt.hover(function(){
+                	clearTimeout(timer);
+                	jQuery(elements).clearQueue();
+                	
+                	elt.animate({ opacity: 1 }, settings.animateTimer);
+                	jQuery(elements).not(elt).animate({ opacity: settings.opacity }, settings.animateTimer);
+                }, function(){
+                	timer = setTimeout(function(){
+                		jQuery(elements).animate({ opacity: 1 }, settings.animateTimer);
+                	}, settings.animateTimer);
+                });
+            });
+		}
+	});
+})(jQuery);
